2e27a8a6f0aa4cdd9aaffa1164a1db748c9538a1
sahithcad01 <ssahithafridi@gmail.com>
2024-01-04 18:09:50 +0530

    Added 04-01-24 files

---
+++ day1-04012024/script.js
@@ -0,0 +1,21 @@
+//Print statement
+console.log("Hello");
+console.log('Hello');
+
+//print number
+var x=10;
+var y=10.250;
+console.log(x);
+console.log(y);
+console.log(typeof(x));
+console.log(typeof(y));
+
+//print string:
+var z='Sahith';
+var z1="Sanjay";
+console.log(z);
+console.log(z1);
+console.log(typeof(z));
+console.log(typeof(z1));
+
+//
