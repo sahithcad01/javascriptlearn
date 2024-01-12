a18db44ec394e92ecd8b4135954bba5676419157
sahithcad01 <ssahithafridi@gmail.com>
2024-01-12 18:09:29 +0530

    day 12-01-2024

--- day6-11012024/registrationform.js
+++ day6-11012024/registrationform.js
@@ -1,1 +1,46 @@
-document.getElementById('username').innerHTML;
+function registerform(){
+    var UserName = document.getElementById("username").value;
+    document.getElementById("tbusername").innerHTML = UserName;
+
+    var FirstName = document.getElementById("firstname").value;
+    document.getElementById("tbfirstname").innerHTML = FirstName;
+
+    var LastName = document.getElementById("lastname").value;
+    document.getElementById("tblastname").innerHTML = LastName;
+
+    var DOB = document.getElementById("dob").value;
+    document.getElementById("tbdob").innerHTML = DOB;
+
+    var Gender =  document.getElementById("male").value;
+    var Gender1 =  document.getElementById("female").value;
+    if(Gender=='Male'){
+        
+        document.getElementById("tbgender").innerHTML = Gender;
+    }
+    else
+    {
+        document.getElementById("tbgender").innerHTML = Gender1;
+    }
+    
+    
+    
+
+    var Qualification = document.getElementById("qualification").value;
+    document.getElementById("tbqualification").innerHTML = Qualification;
+
+    var Location = document.getElementById("location").value;
+    document.getElementById("tblocation").innerHTML = Location;
+
+    var Phone = document.getElementById("phone").value;
+    document.getElementById("tbphone").innerHTML = Phone;
+
+    var Email = document.getElementById("email").value;
+    document.getElementById("tbemail").innerHTML = Email;
+
+    var Address = document.getElementById("address").value;
+    document.getElementById("tbaddress").innerHTML = Address;
+}
+
+function cancel(){
+    alert("Registration Canceled!");
+}
